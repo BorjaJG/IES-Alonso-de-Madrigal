09f0ab2496caf44da79e8998168ae5c22e6cccad
diegogd04 <diego2612004@gmail.com>
2023-02-08 11:52:08 +0100

    crear clase profesor

--- src/domain/models/Profesor.java
+++ src/domain/models/Profesor.java
@@ -1,4 +1,67 @@
 package domain.models;
 
 public class Profesor {
+    private Integer id;
+    private String nombre;
+    private String apellidos;
+    private String dni;
+    private String direccion;
+    private String tituloAcdemico;
+    private String fechaAlta;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTituloAcdemico() {
+        return tituloAcdemico;
+    }
+
+    public void setTituloAcdemico(String tituloAcdemico) {
+        this.tituloAcdemico = tituloAcdemico;
+    }
+
+    public String getFechaAlta() {
+        return fechaAlta;
+    }
+
+    public void setFechaAlta(String fechaAlta) {
+        this.fechaAlta = fechaAlta;
+    }
 }
