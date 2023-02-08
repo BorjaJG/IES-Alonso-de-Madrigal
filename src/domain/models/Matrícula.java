3abf0bac130f175645984526911782ae741b7fbf
diegogd04 <diego2612004@gmail.com>
2023-02-08 11:50:02 +0100

    crear clase matricula

--- src/domain/models/Matrícula.java
+++ src/domain/models/Matrícula.java
@@ -1,4 +1,22 @@
 package domain.models;
 
 public class Matrícula {
+    private Integer id;
+    private String curso;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
 }
